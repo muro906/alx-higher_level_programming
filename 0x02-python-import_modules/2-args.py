b1f6dd1710171211d4b0145ee437f069fe689e62
muro906 <mainamillicent68@gmail.com>
2024-03-17 13:16:08 +0300

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("1: {}".format(sys.argv[1]))
+    
+    elif len(sys.argv) == 1:
+       print("{} arguments.".format(len(sys.argv) - 1))
+    
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)):
+            if i == 0:
+                continue
+            else:
+                print(f"{i}: {sys.argv[i]}")
