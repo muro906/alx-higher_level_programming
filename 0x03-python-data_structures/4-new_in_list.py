e4c0880a32e9c0eaf1ca9d3ea3c1e6f22da57b37
muro906 <mainamillicent68@gmail.com>
2024-03-29 22:44:30 +0300

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if len(my_list) == 0 or idx >= len(my_list):
+    if len(my_list) == 0 or idx >= len(my_list) or idx < 0:
         return my_list.copy()
     new = my_list.copy()
     new.pop(idx)
